c7f2b848ef6879f6e3a0727e60fc1f8ecda7524f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-02-04 22:41:27 -0500

    worked on extension

--- content.js
+++ content.js
@@ -10,3 +10,19 @@ chrome.runtime.sendMessage({
   action: 'retrieveData',
   data: getDataFromLocalStorage()
 })
+
+// content.js
+
+// Listen for messages from background.js
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  // Check if the message contains the expected data
+  if (message && message.message) {
+    // Do something with the data from background.js
+    console.log('Received message in content.js:', message.message)
+
+    // Perform actions based on the data, e.g., modify the DOM
+    document.body.style.backgroundColor = 'lightblue'
+  }
+})
+
+console.log('hi')
