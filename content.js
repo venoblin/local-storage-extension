a99fbeb0152a897416015773b6f16113b4c031f9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-07-14 21:06:31 -0400

    removed background script

--- content.js
+++ content.js
@@ -1,28 +1,5 @@
-// content.js
-
-// Function to retrieve data from local storage
-function getDataFromLocalStorage() {
-  return localStorage.getItem('routine')
+function getLocalStorage() {
+  return localStorage
 }
 
-// Send a message to the background script with the retrieved data
-chrome.runtime.sendMessage({
-  action: 'retrieveData',
-  data: getDataFromLocalStorage()
-})
-
-// content.js
-
-// Listen for messages from background.js
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  // Check if the message contains the expected data
-  if (message && message.message) {
-    // Do something with the data from background.js
-    console.log('Received message in content.js:', message.message)
-
-    // Perform actions based on the data, e.g., modify the DOM
-    document.body.style.backgroundColor = 'lightblue'
-  }
-})
-
-console.log('hi')
+console.log(getLocalStorage())
